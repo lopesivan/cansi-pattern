924f822cae7b6b771a9e24ee34c42a6551125a4b
Ivan Lopes <lopesivan.ufrj@gmail.com>
2025-11-12 05:16:44 -0300

    :style: Atualiza o estilo do arquivo cansi-pattern-state/w/v/models/tStateState_c.py

--- cansi-pattern-state/w/v/models/tStateState_c.py
+++ cansi-pattern-state/w/v/models/tStateState_c.py
@@ -4,6 +4,7 @@ from .factory import Factory
 
 class TStateState_c(Factory):
     """Classe de template."""
+
     def __init__(self, data_model, template_name, index):
 
         Factory.__init__(self, template_name)
@@ -14,15 +15,16 @@ class TStateState_c(Factory):
         self.tmpl.name = "StateState_c"
 
         self.tmpl.state = data_model['state']
-        #self.tmpl.log = data_model['log']
+        # self.tmpl.log = data_model['log']
 
     def put(self):
-        fileName = "%sState.c" % self.tmpl.state['states'][self.tmpl.index]['name'].capitalize()
-        print ("File: %s" % fileName)
-        print self.tmpl
+        fileName = "%sState.c" % self.tmpl.state['states'][self.tmpl.index]['name'].capitalize(
+        )
+        print("File: %s" % fileName)
+        print(self.tmpl)
 
     def save(self):
-        fileName = "%sState.c" % self.tmpl.state['states'][self.tmpl.index]['name'].capitalize()
-        print ("Save File: %s" % fileName)
+        fileName = "%sState.c" % self.tmpl.state['states'][self.tmpl.index]['name'].capitalize(
+        )
+        print("Save File: %s" % fileName)
         open(fileName, 'w').write(str(self.tmpl))
-
