af90070bec77476620f57afe8444baa2eebe6ea7
Ivan Lopes <lopesivan.ufrj@gmail.com>
2025-11-12 05:16:44 -0300

    :style: Atualiza o estilo do arquivo cansi-pattern-state/w/v/models/tState_c.py

--- cansi-pattern-state/w/v/models/tState_c.py
+++ cansi-pattern-state/w/v/models/tState_c.py
@@ -4,6 +4,7 @@ from .factory import Factory
 
 class TState_c(Factory):
     """Classe de template."""
+
     def __init__(self, data_model, template_name):
 
         Factory.__init__(self, template_name)
@@ -16,11 +17,10 @@ class TState_c(Factory):
 
     def put(self):
         fileName = "%sState.c" % self.tmpl.state['name'].capitalize()
-        print ("File: %s" % fileName)
-        print self.tmpl
+        print("File: %s" % fileName)
+        print(self.tmpl)
 
     def save(self):
         fileName = "%sState.c" % self.tmpl.state['name'].capitalize()
-        print ("Save File: %s" % fileName)
+        print("Save File: %s" % fileName)
         open(fileName, 'w').write(str(self.tmpl))
-
