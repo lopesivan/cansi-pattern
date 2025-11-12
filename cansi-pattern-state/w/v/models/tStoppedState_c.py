e955bc83dc39bf374f4d660b7d8beccc5877b10f
Ivan Lopes <lopesivan.ufrj@gmail.com>
2025-11-12 05:16:44 -0300

    :style: Atualiza o estilo do arquivo cansi-pattern-state/w/v/models/tStoppedState_c.py

--- cansi-pattern-state/w/v/models/tStoppedState_c.py
+++ cansi-pattern-state/w/v/models/tStoppedState_c.py
@@ -17,7 +17,7 @@ class TStoppedState_c(Factory):
     def put(self):
         fileName = "%sState.c" %self.tmpl.state['stop']['name'].capitalize()
         print ("File: %s" % fileName)
-        print self.tmpl
+        print (self.tmpl)
 
     def save(self):
         fileName = "%sState.c" %self.tmpl.state['stop']['name'].capitalize()
