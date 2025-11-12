0661f35fc92f9cf57ef0bf63c53198fa62bc8ca6
Ivan Lopes <lopesivan.ufrj@gmail.com>
2025-11-12 05:16:44 -0300

    :style: Atualiza o estilo do arquivo cansi-pattern-state/w/v/models/tStartedState_h.py

--- cansi-pattern-state/w/v/models/tStartedState_h.py
+++ cansi-pattern-state/w/v/models/tStartedState_h.py
@@ -4,6 +4,7 @@ from .factory import Factory
 
 class TStartedState_h(Factory):
     """Classe de template."""
+
     def __init__(self, data_model, template_name):
 
         Factory.__init__(self, template_name)
@@ -15,12 +16,11 @@ class TStartedState_h(Factory):
         self.tmpl.state = data_model['state']
 
     def put(self):
-        fileName = "%sState.h" %self.tmpl.state['start']['name'].capitalize()
-        print ("File: %s" % fileName)
-        print self.tmpl
+        fileName = "%sState.h" % self.tmpl.state['start']['name'].capitalize()
+        print("File: %s" % fileName)
+        print(self.tmpl)
 
     def save(self):
-        fileName = "%sState.h" %self.tmpl.state['start']['name'].capitalize()
-        print ("Save File: %s" % fileName)
+        fileName = "%sState.h" % self.tmpl.state['start']['name'].capitalize()
+        print("Save File: %s" % fileName)
         open(fileName, 'w').write(str(self.tmpl))
-
